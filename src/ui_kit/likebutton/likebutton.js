3c77b4a7782cf024f9eec8cb39c7716c4c557586
aleksei <alekseipeikhel@gmail.com>
2019-06-11 17:00:05 +0300

    Likebutton realized

---
+++ src/ui_kit/likebutton/likebutton.js
@@ -0,0 +1,24 @@
+// get all dropdown blocks as htmlcollection
+const likebuttons = document.getElementsByClassName('likebutton__label');
+// if not exsist dropdown block
+if (likebuttons.length === 0) {
+  // stop script and return
+  return;
+}
+const likebuttonHandler = e => {
+  // get checked status
+  const checked = e.currentTarget.children[0].checked;
+  // if true
+  if(checked) {
+    // increment value
+    e.currentTarget.children[2].innerHTML++
+  } else if(!checked) { // if false
+    // decrement
+    e.currentTarget.children[2].innerHTML--
+  }
+  return;
+}
+// assign every likebutton__label onchange handler
+for (likebutton of likebuttons) {
+  likebutton.onchange = likebuttonHandler;
+}
