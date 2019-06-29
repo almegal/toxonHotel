226b132fe0eb91505128915ce017657cb837a25b
aleksei <alekseipeikhel@gmail.com>
2019-06-29 11:02:03 +0300

    Clean-up

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ import './src/ui_kit/textfield/textfield.js'
 // import './src/ui_kit/likebutton/likebutton.js'
 import './src/ui_kit/calendar/calendar.js'
 import './src/ui_kit/pagination/pagination.js'
+import './src/template/header/header.js'
+const authorize = {
+	login: true,
+	user: 'Test'
+}
+localStorage.setItem('authorize', JSON.stringify(authorize))
