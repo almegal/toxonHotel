1bb6a04a652bb0cddf64d6f3f914a3cbfa1bca3e
aleksei <alekseipeikhel@gmail.com>
2019-07-18 12:18:55 +0300

    Clean-up

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import './src/index.scss'
 // import all js
 import './src/ui_kit/dropdown/dropdown.js';
 import './src/ui_kit/ratestar/ratestar.js'
-// import './src/ui_kit/expendcheckbox/expendedcheckbox.js'
+import './src/ui_kit/expendcheckbox/expendedcheckbox.js'
 import './src/ui_kit/textfield/textfield.js'
-// import './src/ui_kit/likebutton/likebutton.js'
+import './src/ui_kit/likebutton/likebutton.js'
 import './src/ui_kit/calendar/calendar.js'
 import './src/ui_kit/pagination/pagination.js'
-import './src/template/header/header.js'
+//  import js file from template
+import './src/template/includes/header/header.js'
 const authorize = {
 	login: true,
 	user: 'Test'
