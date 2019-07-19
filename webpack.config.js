76fc78eae93a17b60da65e5c4530a1b6c0a0af0d
aleksei <alekseipeikhel@gmail.com>
2019-07-19 06:26:21 +0300

    Webpack config change

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const config = {
 
+// MAIN CONFIG
+// will extends depending on mode
+const config = {
   entry: {
     app: ['./index.js']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
   },
-  devServer: {
-    port: 3000,
-    contentBase: path.join(__dirname, 'src/')
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: './style.css',
-      chunkFilename: '[id].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.pug'
-    }),
-  ],
   module: {
     rules: [
       {
@@ -52,7 +39,7 @@ const config = {
          ],
        },
        {
-        test: /\.(woff(2)?|ttf|eot|png|jpeg|gif|svg|ico)$/,
+        test: /\.(woff(2)?|ttf|eot)$/,
         use: [
           {
             loader: 'file-loader',
@@ -61,17 +48,89 @@ const config = {
             },
           }]
         },
+        {
+          test: /\.(|png|jpeg|jpg|gif|svg|ico)$/,
+          use: [
+                    {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name].[ext]',
+            },
+          }]
+        }
     ]
   }
 };
+// 
+// 
+/*
+{
+  new HtmlWebpackPlugin({
+    template: './src/index.pug',
+  }),
+};
 
+{
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'src/')
+  },
+};
+*/
+const devConfig = {
+    devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'src/')
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+    template: './src/template/views/index.pug',
+    }),
+      new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: './style.css',
+    chunkFilename: '[id].css',
+    // inject: false
+  }),
+  ]
+}
+const prodConfig = {
+    plugins : [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: './style.css',
+        chunkFilename: '[id].css',
+        // inject: false
+    }),
+      new HtmlWebpackPlugin({
+        template: './src/template/views/login.pug',
+        filename: 'login.html'
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/template/views/registration.pug',
+        filename: 'registration.html'
+      }),
+  ]
+}
+// 
+// 
 module.exports = (env, argv) => {
   if (argv.mode === 'development') {
     return {
       ...config,
+      ...devConfig,
       mode: 'development',
       devtool: 'inline-source-map',
     }
   }
-  if (argv.mode === 'production') {}
+  if (argv.mode === 'production') {
+    return {
+      ...config,
+      ...prodConfig,
+      mode: 'production'
+    }
+  }
 }
